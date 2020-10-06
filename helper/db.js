27ca16a5a090127f908ba646372d7d13dc55e511
adilvelizade0 <adilvalizada@gmail.com>
2020-10-06 13:56:14 +0400

    post endpoint

---
+++ helper/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+module.exports = () =>{
+    mongoose.connect("mongodb+srv://velizade0:Adil07112001@myproject.nxzwn.mongodb.net/movie-app-project?retryWrites=true&w=majority", { useNewUrlParser: true,useUnifiedTopology: true })
+    mongoose.connection.on("open",()=>{
+        console.log("MongoDB: Connected");
+    });
+    mongoose.connection.on("error",()=>{
+        console.log("MongoDB: Error",err);
+    })
+    mongoose.Promise = global.Promise;
+}

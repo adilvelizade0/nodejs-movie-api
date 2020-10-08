d5921b291bc5b117c142ebcef0f7cfb540862ec7
adilvelizade0 <adilvalizada@gmail.com>
2020-10-08 15:46:03 +0400

    success movies and directors api

--- models/Movie.js
+++ models/Movie.js
@@ -2,19 +2,43 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const MovieSchema = Schema({
-    director_id: Schema.Types.ObjectId,
-    title: {
-        type:String,
-        required: true,
-    },
-    category: String,
-    country: String,
-    year: Number,
-    imdb_score: Number,
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-})
+  director_id: Schema.Types.ObjectId,
+  title: {
+    type: String,
+    required: [true, "'{PATH}' alani zorunludur!"],
+    maxlength: [
+      15,
+      "'{PATH}' alani ('{VALUE}'), ({MAXLENGTH}) karakterden kucuk olmalidir! ",
+    ],
+    minlength: [
+      4,
+      "'{PATH}' alani ('{VALUE}'), ({MINLENGTH}) karakterden buyuk olmalidir! ",
+    ],
+  },
+  category: {
+    type: String,
+    maxLength: 30,
+    minlength: 1,
+  },
+  country: {
+    type: String,
+    maxLength: 30,
+    minlength: 1,
+  },
+  year: {
+    type: Number,
+    max: 2020,
+    min: 1900,
+  },
+  imdb_score: {
+    type: Number,
+    max: 10,
+    min: 1,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-module.exports = mongoose.model("movie",MovieSchema);
+module.exports = mongoose.model("movie", MovieSchema);

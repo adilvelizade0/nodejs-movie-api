2340819b0528b3c984dfee4459d5b792428ecebf
adilvelizade0 <adilvalizada@gmail.com>
2020-10-09 17:59:19 +0400

    created test

--- helper/db.js
+++ helper/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 module.exports = () =>{
     mongoose.connect("mongodb+srv://velizade0:Adil07112001@myproject.nxzwn.mongodb.net/movie-app-project?retryWrites=true&w=majority", { useNewUrlParser: true,useUnifiedTopology: true })
     mongoose.connection.on("open",()=>{
-        console.log("MongoDB: Connected");
+        // console.log("MongoDB: Connected");
     });
     mongoose.connection.on("error",()=>{
         console.log("MongoDB: Error",err);
